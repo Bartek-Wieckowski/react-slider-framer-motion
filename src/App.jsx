33f79323640adf76ac009d728a6251fb45690de1
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-13 05:16:08 +0200

    changed file to jsx, added motion tag

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion';
+
+function App() {
+  return (
+    <div className="App">
+      <motion.div className="carousel">
+        <motion.div className="carousel-inner"></motion.div>
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
