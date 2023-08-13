31e56de3b9192f1d95634f9af96f650ba4fed059
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-13 05:20:15 +0200

    created images file and export default, used/import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { motion } from 'framer-motion';
+import images from './images';
 
 function App() {
+  console.log(images);
   return (
     <div className="App">
       <motion.div className="carousel">
