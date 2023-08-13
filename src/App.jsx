5a1edbc2beb400e35ddd4a2c40e3fc9ec81e64ca
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-13 05:37:36 +0200

    added dragConstrains right to prevent infinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ function App() {
   return (
     <div className="App">
       <motion.div className="carousel">
-        <motion.div drag="x" className="carousel-inner">
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0 }}
+          className="carousel-inner"
+        >
           {images.map((img, index) => {
             return (
               <motion.div className="item">
